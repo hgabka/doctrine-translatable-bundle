8bc337dfe91c1fae3bbca7ea67caec05644e5105
hgabka <hgabka@gmail.com>
2020-10-23 22:03:46 +0200

    Update HgabkaDoctrineTranslatableExtension.php

--- DependencyInjection/HgabkaDoctrineTranslatableExtension.php
+++ DependencyInjection/HgabkaDoctrineTranslatableExtension.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * (c) Prezent Internet B.V. <info@prezent.nl>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Hgabka\Doctrine\TranslatableBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
