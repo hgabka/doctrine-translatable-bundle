f32c130e962686ea42239ce50f429eb5fe7b952c
hgabka <hgabka@gmail.com>
2020-10-23 21:58:34 +0200

    Update and rename PrezentDoctrineTranslatableBundle.php to HgabkaDoctrineTranslatableBundle.php

---
+++ HgabkaDoctrineTranslatableBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Hgabka\Doctrine\TranslatableBundle;
+
+use Hgabka\Doctrine\TranslatableBundle\DependencyInjection\Compiler\DriverChainCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PrezentDoctrineTranslatableBundle
+ *
+ * @see Bundle
+ */
+class HgabkaDoctrineTranslatableBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DriverChainCompilerPass());
+    }
+}
